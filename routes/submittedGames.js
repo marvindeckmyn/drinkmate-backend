85baff021a0c4f8fd0fb01270ded9ac6e569a3bd
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-06-19 12:41:37 +0200

    Fix approve and reject game

--- routes/submittedGames.js
+++ routes/submittedGames.js
@@ -13,7 +13,7 @@ router.get('/', auth, admin, async (req, res, next) => {
       FROM submitted_games
       JOIN categories on submitted_games.category_id = categories.id
       JOIN users on submitted_games.creator_id = users.id
-      ORDER BY id DESC
+      ORDER BY id ASC
     `);
 
     res.json(submittedGames);
@@ -68,6 +68,82 @@ router.post('/', auth, [
     `, [name, player_count, description, alias, necessities, category_id, creator_id]);
 
     res.json(game);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Approve a game
+router.put('/:id/approve', auth, admin, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    // Start transaction
+    await db.query('BEGIN');
+
+    // Fetch game from submitted_games
+    const { rows: [submittedGame] } = await db.query(`
+      SELECT * FROM submitted_games WHERE id = $1
+    `, [id]);
+
+    // Insert the game to the games table
+    const { rows: [game] } = await db.query(`
+      INSERT INTO games (name, player_count, image, description, alias, category_id, creator_id, publish, new)
+      VALUES($1, $2, $3, $4, $5, $6, $7, false, true)
+      RETURNING *
+    `, [submittedGame.name, submittedGame.player_count, '', submittedGame.description, submittedGame.alias, submittedGame.category_id, submittedGame.creator_id]);
+
+    // Insert the game to game_translations table
+    await db.query(`
+      INSERT INTO game_translations (game_id, language_id, name, alias, description)
+      VALUES ($1, 1, $2, $3, $4)
+    `, [game.id, game.name, game.alias, game.description]);
+
+    // Parse necessities and insert into necessities table
+    const necessities = submittedGame.necessities.split(',');
+    for (let necessity of necessities) {
+      necessity = necessity.trim(); // remove potential extra spaces
+
+      // Insert the necessity to necessities table
+      const { rows: [insertedNecessity] } = await db.query(`
+        INSERT INTO necessities (name, game_id)
+        VALUES ($1, $2)
+        RETURNING *
+      `, [necessity, game.id]);
+
+      // Insert the necessity to necessity_translations table
+      await db.query(`
+        INSERT INTO necessity_translations (necessity_id, language_id, name)
+        VALUES ($1, 1, $2)
+      `, [insertedNecessity.id, necessity]);
+    }
+
+    // Delete the game from submitted_games
+    await db.query(`
+      DELETE FROM submitted_games WHERE id = $1
+    `, [id]);
+
+    // Commit the transaction
+    await db.query('COMMIT');
+
+    res.json(game);
+  } catch (err) {
+    await db.query('ROLLBACK');
+    next(err);
+  }
+});
+
+// Reject a game
+router.delete('/:id/reject', auth, admin, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    // Delete the game from submitted_games
+    await db.query(`
+      DELETE FROM submitted_games WHERE id = $1
+    `, [id]);
+
+    res.json({ message: 'Game rejected and deleted.' });
   } catch (err) {
     next(err);
   }
