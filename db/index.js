63edac6defe9c4e5129bbb76a18054b6601d7277
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-06-21 14:17:43 +0200

    Try this

--- db/index.js
+++ db/index.js
@@ -2,12 +2,12 @@ const { Pool } = require('pg');
 const fs = require('fs');
 const config = require('../config/database');
 
-const pool = new Pool(config);
-
 config.ssl = {
   rejectUnauthorized: false,
   ca: fs.readFileSync('db/ca-certificate.crt').toString(),
 };
+
+const pool = new Pool(config);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
