6ec7889c385668f548c03266b1784d931f4a144b
Marvin Deckmyn <marvindeckmyn@gmail.com>
2023-06-21 13:55:30 +0200

    Add SSL

--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
 const { Pool } = require('pg');
+const fs = require('fs');
 const config = require('../config/database');
 
 const pool = new Pool(config);
+
+config.ssl = {
+  rejectUnauthorized: false,
+  ca: fs.readFileSync('/db/ca-certificate.crt').toString(),
+};
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
